82d0ee8936ce5df67ec5aba3a5026b6d1cd79acf
Max888ceo <maxherscovici@gmail.com>
2026-06-07 21:49:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // ============================================
